5817acba9c869258e3a13e2e18abafbe1dfc8f89
tristan279 <tristandhoedt@gmail.com>
2024-04-26 23:40:06 +0200

    categorizer

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,16 +1,20 @@
 import helmet from "helmet";
-import express, { Application, Request, Response } from "express";
+import express, { Application, Request, Response } from "express"
+import categorize from "./categorize"
 
 const PORT = 8000;
 
 const app: Application = express();
 app.use(helmet());
+app.use(express.json({ limit: "5MB" }));
 
 app.get("/test", function (req: Request, res: Response) {
     return res.json({
         message: "Test succeeded!",
     });
 });
+
+app.use("/categorize", categorize);
 
 const server = app.listen(PORT, () => {
     console.log(`Environment: ${process.env.NODE_ENV}`);
