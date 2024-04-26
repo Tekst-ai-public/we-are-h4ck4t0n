3ae8b2d5ed6ddc2e4aebe1b54241d36eda193203
lancedw <lancedewaele@icloud.com>
2024-04-26 20:59:56 +0200

    login

--- frontend/src/context/authContext.tsx
+++ frontend/src/context/authContext.tsx
@@ -1,17 +1,15 @@
 'use client';
 
-import { AuthLoading } from '@/app/(auth)/components/AuthLoading';
 import { usePathname, useRouter } from 'next/navigation';
-import { ReactNode, createContext, useContext, useEffect, useState } from 'react';
+import { ReactNode, createContext, useContext } from 'react';
+import { AuthLoading } from '@/app/(auth)/components/AuthLoading';
 
 type AuthContextProps = {
-
   login: (email: string, password: string) => Promise<void>;
   logout: () => Promise<void>;
 };
 
 const AuthContext = createContext<AuthContextProps>({
-
   login: async () => {
     throw new Error('Login function not implemented');
   },
@@ -27,29 +25,21 @@ export function useAuth() {
 export function AuthProvider({ children }: { children: ReactNode }) {
   const router = useRouter();
   const pathName = usePathname();
-  const [companyID, setCompanyID] = useState<string | null>(null);
-  const [currentUserName, setCurrentUserName] = useState<string | null>(null);
 
-  async function login(email: string, password: string) {
-    
-  }
+  async function login(email: string, password: string) {}
 
   async function logout() {
     router.push('/login');
-    await ...
   }
 
-  
-
   const accessibleValues: AuthContextProps = {
-
     login,
     logout,
   };
 
   return (
     <AuthContext.Provider value={accessibleValues}>
-      {pathName !== '/login' && !auth.currentUser ? <AuthLoading text="Loading..." /> : children}
+      {pathName !== '/login' || false ? <AuthLoading text="Loading..." /> : children}
     </AuthContext.Provider>
   );
 }
