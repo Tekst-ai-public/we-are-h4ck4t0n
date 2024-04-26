fe1f281febaea4326f9cbf9864383dc5006de8e3
lancedw <lancedewaele@icloud.com>
2024-04-26 22:16:45 +0200

    fix

--- frontend/src/app/(main)/layout.tsx
+++ frontend/src/app/(main)/layout.tsx
@@ -11,15 +11,13 @@ interface MainLayoutProps {
 
 export default function MainLayout({ children, params }: MainLayoutProps) {
     return (
-
-        <div className="flex min-h-screen w-full flex-col bg-muted/40">
-            <div className="flex flex-col sm:gap-4 sm:py-4 sm:pl-14">
-                <Header/>
-                {children}
-            </div>
-
-            <SideNav params={params}>{children}</SideNav>
+      <div className="flex min-h-screen w-full flex-col bg-muted/40">
+        <div className="flex flex-col sm:gap-4 sm:py-4 sm:pl-14">
+          <Header />
+          {children}
         </div>
 
-    )
+        <SideNav params={params}>{children}</SideNav>
+      </div>
+    );
 }
