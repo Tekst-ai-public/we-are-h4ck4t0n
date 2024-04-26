05d765514f906d82b4cd2f4609cb89345f2819dc
lancedw <lancedewaele@icloud.com>
2024-04-26 22:18:28 +0200

    fix layout

--- frontend/src/app/(main)/layout.tsx
+++ frontend/src/app/(main)/layout.tsx
@@ -5,19 +5,17 @@ import SideNav from "@/app/(main)/components/sideNav";
 import Header from "@/app/(main)/components/header";
 
 interface MainLayoutProps {
-    children: React.ReactNode
-    params: { domain: string }
+  children: React.ReactNode;
 }
 
-export default function MainLayout({ children, params }: MainLayoutProps) {
-    return (
-      <div className="flex min-h-screen w-full flex-col bg-muted/40">
-        <div className="flex flex-col sm:gap-4 sm:py-4 sm:pl-14">
-          <Header />
-          {children}
-        </div>
-
-        <SideNav params={params}>{children}</SideNav>
+export default function MainLayout({ children }: MainLayoutProps) {
+  return (
+    <div className="flex min-h-screen w-full flex-col bg-muted/40">
+      <div className="flex flex-col sm:gap-4 sm:py-4 sm:pl-14">
+        <Header />
+        {children}
       </div>
-    );
+      <SideNav />
+    </div>
+  );
 }
