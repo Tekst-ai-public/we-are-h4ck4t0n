c6a2a8015e3fc180d634a5fcaa7a875d6282a3a5
Julien Van Beveren <julien.vanbeveren@gmail.com>
2024-04-27 11:30:47 +0200

    ready

--- frontend/src/app/(main)/[pageId]/dashboard/page.tsx
+++ frontend/src/app/(main)/[pageId]/dashboard/page.tsx
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from 'react';
 import { BarChart4 } from 'lucide-react';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import { useAuth } from "@/context/authContext";
+import { Select, SelectContent, SelectGroup, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
 
 type CommentTypeCount = {
   type: string;
@@ -20,10 +21,11 @@ export default function Page({ params }: { params: { pageId: string } }) {
   const [numbersData, setNumbersData] = useState<NumbersData | null>(null);
   const [isNumbersLoading, setIsNumbersLoading] = useState<boolean>(true);
 
-  const [labels, setLabels] = useState([])
+  const [labels, setLabels] = useState<any[]>([])
+  const [selectedLabel, setSelectedLabel] = useState("")
   const [labelsLoading, setLabelsLoading] = useState(true)
 
-
+  const [comments, setComments] = useState<{ authorId: string, content: string, createdAt: string, id: string, meta: { comment_type: string }, postId: string, updatedAt: string }[]>([])
 
   const { apiFetch } = useAuth()
 
@@ -33,7 +35,7 @@ export default function Page({ params }: { params: { pageId: string } }) {
         const response = await apiFetch<any>(`/page/settings?pageId=${params.pageId}`, {});
         const data = await response.json()
         if (response.ok) {
-          setLabels(data.prompt?.labels || [])
+          setLabels(["all", ...(data.prompt?.labels || [])])
           setLabelsLoading(false)
         }
       } catch (error) {
@@ -44,6 +46,35 @@ export default function Page({ params }: { params: { pageId: string } }) {
 
     fetchData();
   }, []);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const searchParams = new URLSearchParams({
+          pageId: params.pageId
+        })
+        if (selectedLabel !== "all") {
+          searchParams.set("label", selectedLabel)
+        }
+        const response = await apiFetch(`/comments?${searchParams.toString()}`, {});
+        setComments(await response.json() as any);
+      } catch (error) {
+        console.log("numbers failed")
+      } finally {
+        setIsNumbersLoading(false);
+      }
+    };
+
+    const interval = setInterval(() => {
+      fetchData()
+    }, 5000)
+
+    fetchData();
+
+    return () => {
+      clearInterval(interval)
+    }
+  }, [selectedLabel]);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -88,26 +119,28 @@ export default function Page({ params }: { params: { pageId: string } }) {
         </CardHeader>
         <CardContent>{isNumbersLoading ? "Loading ..." : JSON.stringify(numbersData?.commentTypeCounts || 0)}</CardContent>
       </Card>
-      <Card className="col-span-2 h-96">
-        <CardHeader>
-          <CardTitle>Feed</CardTitle>
-        </CardHeader>
-        <CardContent>Posts</CardContent>
-      </Card>
-      <Card className="col-span-2">
-        <CardHeader>
-          <CardTitle>A beautiful graph</CardTitle>
-        </CardHeader>
-        <CardContent>
-          <BarChart4 />
-        </CardContent>
-      </Card>
       <Card className="col-span-4 h-[500px]">
-        <CardHeader>
-          <CardTitle>Live comments</CardTitle>
+        <CardHeader className="flex flex-row justify-between">
+          <CardTitle>Live Feed</CardTitle>
+          <Select value={selectedLabel} onValueChange={newValue => setSelectedLabel(newValue)}>
+            <SelectTrigger className="w-[350px]">
+              <SelectValue placeholder="select a label" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectGroup>
+                {labels.map((label: any) => {
+                  return <SelectItem key={label} value={label}>{label}</SelectItem>
+                })}
+              </SelectGroup>
+            </SelectContent>
+          </Select>
         </CardHeader>
-        <CardContent>
-          <BarChart4 />
+        <CardContent className="flex flex-col gap-3">
+          {comments.map(comment => {
+            return <div className="rounded-md border py-2 px-3" key={comment.id}>
+              <div>{comment.content}</div>
+            </div>
+          })}
         </CardContent>
       </Card>
     </div>
