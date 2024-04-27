545b9374f0ecc7332e54c7ef630bd38651b490d0
Julien Van Beveren <julien.vanbeveren@gmail.com>
2024-04-27 09:09:00 +0200

    done

--- backend/src/api/posts/numbers.ts
+++ backend/src/api/posts/numbers.ts
@@ -1,43 +1,63 @@
-import express, {NextFunction, Request, Response} from "express";
+import express, { NextFunction, Request, Response } from "express";
 import prisma from "../../utils/prisma";
+import { authMiddleware } from "../../utils/authMiddleware";
 const app = express.Router();
 
-app.get('/', async function(req: Request, res: Response, next: NextFunction) {
-    try {
-        const postCount = await prisma.posts.count();
+app.get('/', authMiddleware(), async function(req: Request, res: Response, next: NextFunction) {
+  try {
 
-        // Count of all comments
-        const commentCount = await prisma.comments.count();
+    const pageId = req.query.pageId as string
 
-        // Count comments by `comment_type`
-        const commentTypeCounts = await prisma.comments.groupBy({
-            by: ['meta'],
-            _count: {
-                _all: true,
-            },
-            where: {
-                meta: {
-                    path: ['comment_type'],
-                    not: null,
-                },
-            },
-        });
+    const postCount = await prisma.posts.count({
+      where: {
+        pageId: pageId
+      }
+    });
 
-        // Preparing the result
-        const result = {
-            postCount,
-            commentCount,
-            commentTypeCounts: commentTypeCounts.map(item => ({
-                type: item.meta?.comment_type,
-                count: item._count._all,
-            })),
-        };
+    // Count of all comments
+    const commentCount = await prisma.comments.count({
+      where: {
+        post: {
+          pageId: pageId
+        }
+      }
+    });
 
-        return res.status(200).json(result);
-    } catch (error) {
-        console.error(error)
-        next(error);
-    }
+    // Count comments by `comment_type`
+    const commentTypeCounts = await prisma.comments.groupBy({
+      by: ['meta'],
+      _count: {
+        _all: true,
+      },
+      where: {
+        meta: {
+          path: ['comment_type'],
+          // @ts-ignore
+          not: null
+        },
+        post: {
+          pageId: pageId
+        }
+      },
+    });
+
+    // Preparing the result
+    const result = {
+      postCount,
+      commentCount,
+      commentTypeCounts: commentTypeCounts.map(item => ({
+        // @ts-ignore
+        type: (item.meta?.comment_type) as string,
+        // @ts-ignore
+        count: item._count._all,
+      })),
+    };
+
+    return res.status(200).json(result);
+  } catch (error) {
+    console.error(error)
+    next(error);
+  }
 });
 
 export default app;
